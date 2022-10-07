8738fce14c9b583cbb2f746ad9285be52e5612ca
Blubywaff <45488563+Blubywaff@users.noreply.github.com>
2022-10-07 12:08:59 -0500

    add coc.nvim for enhanced code features

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,11 @@
 require('treesitter-context')
+require('coc')
 -- Packer setup
-return require('packer').startup(function(use)
+require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'folke/tokyonight.nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'nvim-treesitter/nvim-treesitter-context'
+    use {'neoclide/coc.nvim', branch='release'}
 end)
 
